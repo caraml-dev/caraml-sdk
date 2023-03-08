36d5a417389bd963d825d89a633771cadc44af08
ewezy <ziyi.ewe@gojek.com>
2023-03-08 16:59:30 +0800

    Fix typo in version file

--- packages/caraml-auth-google/version.py
+++ packages/caraml-auth-google/version.py
@@ -1,2 +1,2 @@
-# Do not update this field manually; this value gets updated automically during build time
+# Do not update this field manually; this value gets updated automatically during build time
 VERSION = "0.0.0"
