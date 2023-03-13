965a2e5cd397d03515fd96b6ef260c4b6b70cc5d
Ewe Zi Yi <36802364+deadlycoconuts@users.noreply.github.com>
2023-03-13 17:59:35 +0800

    Add caraml-auth-google publish workflow (#2)
    * Add caraml-auth-google publish workflow
    
    * Add release rules action
    
    * Setting version number to default
    
    * Fix prefix value for release rules
    
    * Refactor github workflow
    
    * Fix typo in version file

--- packages/caraml-auth-google/version.py
+++ packages/caraml-auth-google/version.py
@@ -1,3 +1,2 @@
 # Do not update this field manually; this value gets updated automatically during build time
-# THIS IS CURRENTLY SET MANUALLY AS THE CICD PIPELINE DOES NOT CURRENTLY PUBLISH THE PACKAGE.
-VERSION = "0.0.1"
+VERSION = "0.0.0"
